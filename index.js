9261f018c6cbd7e454ca22f061c3c2159a35e587
Henri Normak <henri.normak@gmail.com>
2016-03-30 13:47:51 +0300

    No quotes

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ module.exports = {
         'space-infix-ops': [2],
         'space-unary-ops': [2],
         'spaced-comment': [2, 'always'],
-        'strict': [2, 'safe'],
+        strict: [2, 'safe'],
         yoda: [2]
     },
     env: {
